80a78ddf761487c26f5103ca54106478fcf512e9
Alex <temper8@gmail.com>
2026-04-17 17:04:40 +0300

    Update viewer.py

--- viewer.py
+++ viewer.py
@@ -3,7 +3,6 @@ from matplotlib.colors import LogNorm
 import numpy as np
 
 from reader import dataset_reader
-# Укажите путь к вашему HDF5 файлу
 
 def plot_Ea(X,Y,Z):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
@@ -17,7 +16,6 @@ def plot_Ea(X,Y,Z):
     ax2.set_title('Ea (LogNorm)')
     fig.savefig('double_disk.png')  
     plt.show()
-    print("\nДанные успешно загружены в переменную 'data_array'")
 
 def plot_complex_split(X, Y, Z, title="Z"):
     """plot two graph: magnitude and phase."""
@@ -29,7 +27,7 @@ def plot_complex_split(X, Y, Z, title="Z"):
     ax1.set_aspect('equal')
     ax1.set_title(f'Magnitude |{title}|')
 
-    # phase (Аргумент)
+    # phase (arg)
     phase = np.angle(Z) # Значения от -pi до pi
     im2 = ax2.pcolormesh(X, Y, phase, cmap='twilight', shading='gouraud')
     fig.colorbar(im2, ax=ax2, label='Phase (radians)')
@@ -44,12 +42,18 @@ def plot_complex_domain(X, Y, Z):
 
 file_path = 'data/results.h5'
                                 # Имя 2D массива внутри HDF5-файла
-X =  dataset_reader(file_path, '/nphi-122/grid_2d/X')
-Y =  dataset_reader(file_path, '/nphi-122/grid_2d/Y')
-Ea = dataset_reader(file_path, '/nphi-122/field_2d/Ea')
-Ex = dataset_reader(file_path, '/nphi-122/field_2d/Ex')
-Ey = dataset_reader(file_path, '/nphi-122/field_2d/Ey')
-Ez = dataset_reader(file_path, '/nphi-122/field_2d/Ez')
+nphi = "-012"
+X =  dataset_reader(file_path, f'/nphi{nphi}/grid_2d/X')
+Y =  dataset_reader(file_path, f'/nphi{nphi}/grid_2d/Y')
+Ea = dataset_reader(file_path, f'/nphi{nphi}/field_2d/Ea')
 
 plot_Ea(X,Y,Ea)
+
+Ex = dataset_reader(file_path, f'/nphi{nphi}/field_2d/Ex')
 plot_complex_split(X, Y, Ex, "Ex")
+
+Ey = dataset_reader(file_path, f'/nphi{nphi}/field_2d/Ey')
+plot_complex_split(X, Y, Ey, "Ey")
+
+Ez = dataset_reader(file_path, f'/nphi{nphi}/field_2d/Ez')
+plot_complex_split(X, Y, Ey, "Ez")
